838d0598763e275dd4d2a1ecc00462c5a8513db1
agatpan <Krokodyl83>
2017-12-28 21:25:38 +0100

    Pupil commit first

--- Marks.java
+++ Marks.java
@@ -1,5 +1,10 @@
 public class Marks {
     public static void main(String[] args){
         System.out.print("Hello World");
+
+        Pupil pupil = new Pupil("Agata", "Pan",[6];
+        Pupil pupil_1 = new Pupil("Agata", "Pan",{4.0, 3.0, 3.5, 5.0, 4.5});
+        System.out.println(pupil);
+        System.out.println(pupil_1);
     }
 }
---
+++ Pupil.java
@@ -0,0 +1,33 @@
+public class Pupil {
+
+    public String firstName;
+    public String secondName;
+    public float[] marks;
+    public float sumMark;
+    public float averegeMark;
+
+
+
+    public Pupil (String firstName, String secondName, float[] marks){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.marks = marks;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public float[] getMarks() {
+        return marks;
+    }
+    public float averageMark() {
+        returna averageMark;
+
+
+    }
+}
